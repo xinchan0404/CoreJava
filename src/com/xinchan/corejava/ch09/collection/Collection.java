7d51e3821e4e195035bda20bf782161143f3827a
xinchan <zhengxin2021@gmail.com>
2023-02-09 22:18:20 +0800

    corejava - 0209

---
+++ src/com/xinchan/corejava/ch09/collection/Collection.java
@@ -0,0 +1,11 @@
+package com.xinchan.corejava.ch09.collection;
+
+/**
+ * @author xinchan
+ * @version 1.0.1 2023-02-09
+ */
+public class Collection {
+    public static void main(String[] args) {
+
+    }
+}
